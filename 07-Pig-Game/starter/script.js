e2fe3945609cb86a912c4694f4bf068917fdbf9c
Anastasia DROKINA <adrokina@logisneuf.prive>
2021-01-14 16:24:08 +0100

    Add switch style to next player

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const current0El = document.querySelector('#current--0');
@@ -16,7 +18,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 1]
 let curentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -26,16 +30,18 @@ btnRoll.addEventListener('click', function () {
   // 2. Display dice
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
+
   // 3. Check for rolled 1: if true, switch to next player
   if (dice !== 1) {
     // Add dice to curent score
     curentScore = curentScore + dice;
-    current0El.textContent = curentScore; // TODO Change later
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
   } else {
     // Switch to next player
-    if (dice === 1) {
-      curentScore = curentScore + dice;
-      current1El.textContent = curentScore; // TODO Change later
-    }
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;  
+    player0El.classList.toggle('.player--active');
+    player1El.classList.toggle('.player--active');
   }
 });
