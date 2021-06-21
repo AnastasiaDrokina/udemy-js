126fdcc587988d0d76f708738d6e4cbafa7a0984
AnastasiaDrokina <46569895+AnastasiaDrokina@users.noreply.github.com>
2021-06-21 11:32:24 +0200

    Closures

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -199,3 +199,73 @@
 // const addVAT2 = addTaxRate(0.23);
 // console.log(addVAT2(100));
 // console.log(addVAT2(23));
+
+// //  Immediately Invoked Function Expressions (IIFE)
+// const runOnce = function () {
+//   console.log('This will never run again');
+// };
+// runOnce();
+
+// // IIFE
+// (function () {
+//   console.log('This will never run again');
+// })();
+
+// (() => console.log('This will ALSO never run again'))();
+
+// //  Closures
+// const secureBooking = function () {
+//   let passengerCount = 0;
+//   return function () {
+//     passengerCount++;
+//     console.log(`${passengerCount} passengers`);
+//   };
+// };
+
+// const booker = secureBooking();
+// booker();
+// booker();
+// booker();
+
+// console.dir(booker);
+
+// // Example 1
+// let f;
+
+// const g = function () {
+//   const a = 23;
+//   f = function () {
+//     console.log(a * 2);
+//   };
+// };
+
+// const h = function () {
+//   const b = 777;
+//   f = function () {
+//     console.log(b * 2);
+//   };
+// };
+
+// g();
+// f();
+
+// // Re-assigning f function
+// h();
+// f();
+// console.log(f);
+
+// // Emample 2
+// const boarPassengers = function (n, wait) {
+//   const perGroup = n / 3;
+
+//   setTimeout(function () {
+//     console.log(`Will are now baording all
+//     ${n} passengers`);
+//     console.log(`There are 3 groupes, each with ${perGroup} passengers`);
+//   }, wait * 1000);
+
+//   console.log(`Will start boarding in
+//   ${wait} seconds`);
+// };
+
+// boarPassengers(180, 3);
